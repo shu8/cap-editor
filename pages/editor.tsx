5e3296ca97ffb842445bddb66e2c97344be530c5
Shubham Jain <shubham@sjain.dev>
2022-12-23 20:38:42 +0000

    Update editor page to use new CAP Editor UI

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,11 +1,26 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useSession } from "next-auth/react";
-import { Button, Form } from "rsuite";
+import NewAlert, { AlertData } from "../components/cap/NewAlert";
+import prisma from "../lib/db";
+import { unstable_getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
 
-export default function Home() {
-  const { data: session } = useSession();
-  console.log(session);
+export const getServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  if (!session) return { props: { session } };
+
+  const alertingAuthority = await prisma.alertingAuthority.findFirst({
+    where: { users: { some: { email: session.user.email } } },
+  });
+  console.log(session, alertingAuthority);
+  return { props: { session, alertingAuthority } };
+};
+
+export default function Home({ session, alertingAuthority }) {
+  console.log(session, alertingAuthority);
 
   return (
     <>
@@ -15,89 +30,34 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Form
-          onSubmit={(_, e) => {
-            e.preventDefault();
-
-            const formData = new FormData(e.target as HTMLFormElement);
-            const formProps = Object.fromEntries(formData);
-
+      <main>
+        <p>{session ? "You are logged in" : "You are not logged in"}</p>
+        <NewAlert
+          alertingAuthority={alertingAuthority}
+          onSubmit={async (alertData: AlertData) => {
             fetch("/api/alerts", {
               method: "POST",
               headers: { "Content-Type": "application/json" },
               body: JSON.stringify({
-                sender: formProps.email,
                 status: formProps.status,
                 msgType: formProps.msgType,
                 scope: formProps.scope,
                 info: {
-                  category: formProps.category,
-                  event: formProps.event,
-                  urgency: formProps.urgency,
-                  severity: formProps.severity,
-                  certainty: formProps.certainty,
-                  resourceDesc: formProps.resourceDesc,
-                  areaDesc: formProps.areaDesc,
+                  category: alertData.category,
+                  responseType: alertData.actions,
+                  urgency: alertData.urgency,
+                  severity: alertData.severity,
+                  certainty: alertData.certainty,
+                  onset: alertData.from,
+                  expires: alertData.to,
+                  headline: alertData.headline,
+                  description: alertData.description,
+                  instruction: alertData.instruction,
                 },
               }),
             });
           }}
-        >
-          <Form.Group>
-            <Form.ControlLabel>Sender</Form.ControlLabel>
-            <Form.Control
-              name="email"
-              type="email"
-              placeholder="me@example.com"
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Status</Form.ControlLabel>
-            <Form.Control name="status" value="Test" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Message Type</Form.ControlLabel>
-            <Form.Control name="msgType" value="Alert" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Scope</Form.ControlLabel>
-            <Form.Control name="scope" value="Public" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Category</Form.ControlLabel>
-            <Form.Control name="category" value="Met" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Event</Form.ControlLabel>
-            <Form.Control name="event" value="flood" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Urgency</Form.ControlLabel>
-            <Form.Control name="urgency" value="expected" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Severity</Form.ControlLabel>
-            <Form.Control name="severity" value="Minor" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Certainty</Form.ControlLabel>
-            <Form.Control name="certainty" value="Possible" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Resource Description</Form.ControlLabel>
-            <Form.Control name="resourceDesc" value="Image file" />
-          </Form.Group>
-          <Form.Group>
-            <Form.ControlLabel>Area Description</Form.ControlLabel>
-            <Form.Control name="areaDesc" value="London, England, UK" />
-          </Form.Group>
-          <Form.Group>
-            <Button appearance="primary" type="submit">
-              Next
-            </Button>
-          </Form.Group>
-        </Form>
+        />
       </main>
     </>
   );
