dee73df064844572780c7f23fc7b45dc70facda5
Shubham Jain <shubham@sjain.dev>
2022-12-29 23:28:24 +0000

    Improve summary step style

--- components/editor/steps/SummaryStep.tsx
+++ components/editor/steps/SummaryStep.tsx
@@ -4,9 +4,14 @@ import styles from "../../../styles/components/cap/Step.module.css";
 import { IconButton } from "rsuite";
 import EditIcon from "@rsuite/icons/Edit";
 
-const ReviewItem = (props: { field: string; value: string }) => (
+const ReviewItem = (props: {
+  field: string;
+  newLine?: boolean;
+  value: string;
+}) => (
   <div className={classes(styles.reviewItem)}>
     <strong>{props.field}: </strong>
+    {props.newLine && <br />}
     {props.value}
   </div>
 );
@@ -36,51 +41,79 @@ export default function SummaryStep({
       <p>
         Please review and confirm the alert&apos;s information presented below.
       </p>
-      <Header step="metadata" />
-      <ReviewItem field="Status" value={alertData.status ?? "NONE"} />
-      <ReviewItem field="Message Type" value={alertData.msgType ?? "NONE"} />
-      <ReviewItem field="Scope" value={alertData.scope ?? "NONE"} />
-      <ReviewItem field="Restriction" value={alertData.restriction ?? "NONE"} />
-      <ReviewItem
-        field="Addresses"
-        value={alertData.addresses?.join("; ") ?? "NONE"}
-      />
-      <ReviewItem
-        field="References"
-        value={alertData.references?.join("; ") ?? "NONE"}
-      />
+      <div className={styles.reviewStepsWrapper}>
+        <div className={styles.reviewStep}>
+          <Header step="metadata" />
+          <ReviewItem field="Status" value={alertData.status ?? "NONE"} />
+          <ReviewItem
+            field="Message Type"
+            value={alertData.msgType ?? "NONE"}
+          />
+          <ReviewItem field="Scope" value={alertData.scope ?? "NONE"} />
+          <ReviewItem
+            field="Restriction"
+            value={alertData.restriction ?? "NONE"}
+          />
+          <ReviewItem
+            field="Addresses"
+            value={alertData.addresses?.join("; ") ?? "NONE"}
+          />
+          <ReviewItem
+            field="References"
+            value={alertData.references?.join("; ") ?? "NONE"}
+          />
+        </div>
 
-      <Header step="category" />
-      <ReviewItem
-        field="Alert Category"
-        value={alertData.category?.join(", ") ?? "NONE"}
-      />
-      <ReviewItem field="Event" value={alertData.event ?? "NONE"} />
+        <div className={styles.reviewStep}>
+          <Header step="category" />
+          <ReviewItem
+            field="Alert Category"
+            value={alertData.category?.join(", ") ?? "NONE"}
+          />
+          <ReviewItem field="Event" value={alertData.event ?? "NONE"} />
+        </div>
+        <div className={styles.reviewStep}>
+          <Header step="map" />
+          <ReviewItem
+            field="Regions"
+            value={Object.keys(alertData.regions ?? {}).join(", ") ?? "NONE"}
+          />
+        </div>
 
-      <Header step="map" />
-      <ReviewItem
-        field="Regions"
-        value={Object.keys(alertData.regions ?? {}).join(", ") ?? "NONE"}
-      />
+        <div className={styles.reviewStep}>
+          <Header step="data" />
+          <ReviewItem
+            field="From"
+            value={alertData.from?.toLocaleString() ?? "NONE"}
+          />
+          <ReviewItem
+            field="To"
+            value={alertData.to?.toLocaleString() ?? "NONE"}
+          />
+          <ReviewItem field="Severity" value={alertData.severity ?? "NONE"} />
+          <ReviewItem field="Certainty" value={alertData.certainty ?? "NONE"} />
+          <ReviewItem field="Urgency" value={alertData.urgency ?? "NONE"} />
+        </div>
 
-      <Header step="data" />
-      <ReviewItem
-        field="From"
-        value={alertData.from?.toLocaleString() ?? "NONE"}
-      />
-      <ReviewItem field="To" value={alertData.to?.toLocaleString() ?? "NONE"} />
-      <ReviewItem field="Severity" value={alertData.severity ?? "NONE"} />
-      <ReviewItem field="Certainty" value={alertData.certainty ?? "NONE"} />
-      <ReviewItem field="Urgency" value={alertData.urgency ?? "NONE"} />
-
-      <Header step="text" />
-      <ReviewItem field="Headline" value={alertData.headline ?? "NONE"} />
-      <ReviewItem field="Description" value={alertData.description ?? "NONE"} />
-      <ReviewItem field="Instruction" value={alertData.instruction ?? "NONE"} />
-      <ReviewItem
-        field="Actions"
-        value={alertData.actions?.join(", ") ?? "NONE"}
-      />
+        <div className={styles.reviewStep}>
+          <Header step="text" />
+          <ReviewItem field="Headline" value={alertData.headline ?? "NONE"} />
+          <ReviewItem
+            field="Description"
+            newLine
+            value={alertData.description ?? "NONE"}
+          />
+          <ReviewItem
+            field="Instruction"
+            newLine
+            value={alertData.instruction ?? "NONE"}
+          />
+          <ReviewItem
+            field="Actions"
+            value={alertData.actions?.join(", ") ?? "NONE"}
+          />
+        </div>
+      </div>
     </div>
   );
 }
