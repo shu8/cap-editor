9cbd390a5ab322207f4c42054290b78b95ae43f3
Shubham Jain <shubham@sjain.dev>
2023-02-04 23:25:06 +0000

    Add missing options to toaster.push wrapper

--- lib/useToasterI18n.tsx
+++ lib/useToasterI18n.tsx
@@ -1,12 +1,19 @@
 import { useToaster } from "rsuite";
 import { I18nProvider } from "@lingui/react";
 import { i18n } from "@lingui/core";
+import { ToastContainerProps } from "rsuite/esm/toaster/ToastContainer";
 
 export const useToasterI18n = () => {
   const originalUseToaster = useToaster();
   return {
     ...originalUseToaster,
-    push: (node: React.ReactNode) =>
-      originalUseToaster.push(<I18nProvider i18n={i18n}>{node}</I18nProvider>),
+    push: (
+      node: React.ReactNode,
+      options: ToastContainerProps | undefined = undefined
+    ) =>
+      originalUseToaster.push(
+        <I18nProvider i18n={i18n}>{node}</I18nProvider>,
+        options
+      ),
   };
 };
