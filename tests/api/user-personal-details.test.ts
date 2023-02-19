4c13381b92971e67893cde0be9c0be83225adc80
Shubham Jain <shubham@sjain.dev>
2023-02-19 20:29:32 +0000

    Add API tests for personal details

---
+++ tests/api/user-personal-details.test.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { expect, test, describe, jest } from "@jest/globals";
+import { createMocks } from "node-mocks-http";
+import handlePersonalDetails from "../../pages/api/user/personalDetails";
+import { prismaMock } from "./setup";
+import { createUser, mockUserOnce, users } from "./helpers";
+
+jest.mock("next-auth/react");
+jest.mock("next-auth");
+describe("/api/user/personalDetails", () => {
+  test("must be logged in", async () => {
+    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
+      method: "POST",
+      body: { name: "Name" },
+    });
+    await handlePersonalDetails(req, res);
+    expect(res._getStatusCode()).toEqual(401);
+  });
+
+  test("valid name must be supplied", async () => {
+    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
+      method: "POST",
+    });
+    mockUserOnce(users.admin);
+    await handlePersonalDetails(req, res);
+    expect(res._getStatusCode()).toEqual(400);
+  });
+
+  test("user is updated in db", async () => {
+    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
+      method: "POST",
+      body: { name: "Updated" },
+    });
+    await createUser();
+    mockUserOnce(users.admin);
+    await handlePersonalDetails(req, res);
+    const user = await prismaMock.user.findFirst({
+      where: { email: users.admin.email },
+    });
+    expect(user?.name).toEqual("Updated");
+    expect(res._getStatusCode()).toEqual(200);
+  });
+});
