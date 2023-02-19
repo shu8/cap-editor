be8faff0bbe232ab15436d6add4786d0ad128d8f
Shubham Jain <shubham@sjain.dev>
2023-02-19 20:23:14 +0000

    Update API tests (register)

--- tests/api/register.test.ts
+++ tests/api/register.test.ts
@@ -13,15 +13,6 @@ describe("/api/register", () => {
     expect(res._getStatusCode()).toEqual(400);
   });
 
-  test("valid AA must be supplied", async () => {
-    const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
-      method: "POST",
-      body: { email: "test@example.com", name: "test" },
-    });
-    await handleRegister(req, res);
-    expect(res._getStatusCode()).toEqual(400);
-  });
-
   test("valid name must be supplied", async () => {
     const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
       method: "POST",
@@ -43,16 +34,13 @@ describe("/api/register", () => {
   test("user is created in db", async () => {
     const { req, res } = createMocks<NextApiRequest, NextApiResponse>({
       method: "POST",
-      body: {
-        alertingAuthorityId: "aa",
-        name: "test",
-        email: "test@example.com",
-      },
+      body: { name: "test", email: "test@example.com" },
     });
     await handleRegister(req, res);
     const users = await prismaMock.user.findMany();
     expect(users.length).toEqual(1);
-    expect(users[0].alertingAuthorityVerificationToken).toBeTruthy();
+    expect(users[0].name).toEqual("test");
+    expect(users[0].email).toEqual("test@example.com");
     expect(res._getStatusCode()).toEqual(200);
   });
 });
